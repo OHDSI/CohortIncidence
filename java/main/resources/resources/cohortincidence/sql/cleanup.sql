449a68c1966df5a2f6d915e0855cd60632da8223
Chris Knoll <cknoll@ohdsi.org>
2024-07-01 10:12:40 -0400

    Refactored results table into core and def tables.

--- java/main/resources/resources/cohortincidence/sql/cleanup.sql
+++ java/main/resources/resources/cohortincidence/sql/cleanup.sql
@@ -1,1 +1,6 @@
 DROP TABLE @schemaName.incidence_summary;
+DROP TABLE @schemaName.target_def;
+DROP TABLE @schemaName.outcome_def;
+DROP TABLE @schemaName.tar_def;
+DROP TABLE @schemaName.age_group_def;
+DROP TABLE @schemaName.subgroup_def;
